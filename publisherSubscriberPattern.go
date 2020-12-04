0139a3c6022edbd88ba45e6f4a488a646a751298
Larsen Close <larsenclose@protonmail.com>
2020-12-04 13:10:30 -0700

    printing, reaching deadlock mid program

--- publisherSubscriberPattern.go
+++ publisherSubscriberPattern.go
@@ -17,71 +17,68 @@ import (
 // PubSub is our struct for the publisher-subscriber pattern
 type PubSub struct {
 	topics map[string][]chan string
-	mu sync.Mutex
+	mu     sync.Mutex
 }
-
-
 
 var wg sync.WaitGroup
 
 // TODO: creates and returns a new channel on a given topic, updating the PubSub struct
-func (ps *PubSub) subscribe(topic string, name string) chan string {
-	//ps.mu.Lock()
+func (ps *PubSub) subscribe(topic string) chan string {
+	ps.mu.Lock()
 	//defer ps.mu.Unlock()
 	ch := make(chan string)
 	ps.topics[topic] = append(ps.topics[topic], ch)
+	ps.mu.Unlock()
 	return ch
 }
 
 // TODO: writes the given message on all the channels associated with the given topic
 func (ps *PubSub) publish(topic string, msg string) {
-	//ps.mu.Lock()
+	ps.mu.Lock()
 	//defer ps.mu.Unlock()
 	for _, ch := range ps.topics[topic] {
-		ch <- msg
+		go func(channel chan string) {
+			channel <- msg
+		}(ch)
 	}
+	ps.mu.Unlock()
 }
 
 // TODO: sends messages taken from a given array of message, one at a time and at random intervals, to all topic subscribers
-func  publisher(ps PubSub, topic string, msgs [5]string) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
+func (ps *PubSub) publisher(topic string, msgs [5]string) {
+	// ps.mu.Lock()
+	// defer ps.mu.Unlock()
 
 	for message := range msgs {
 		ps.publish(topic, msgs[message])
 		time.Sleep(time.Duration(rand.Intn(4)) * time.Second)
 	}
-	wg.Done()
-}
 
+}
 
 // TODO: reads and displays all messages received from a particular topic
-func  subscriber(ps PubSub, name string, topic string) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
+func (ps *PubSub) subscriber(name string, topic string) {
+	// ps.mu.Lock()
+	// defer ps.mu.Unlock()
 
-	 ch = ps.subscribe(topic, name)
-
-	for topic, slice := range ps.topics {
-		go printer(topic, slice)
-
+	ch := ps.subscribe(topic)
+	for range ch {
+		go printer(name, topic, ch)
 	}
 }
 
-func printer(topic string, channel []chan string,) {
-	for item := range channel {
-		var receive = <-channel[item]
-		fmt.Printf("Topic: %v; Message: %v\n", topic, receive)
+func printer(name string, topic string, channel chan string) {
+	for {
+		var recieve = <-channel
+		fmt.Printf("Subcriber: %v, Topic: %v; Message: %v\n", name, topic, recieve)
 	}
+
 }
 
 func main() {
 
 	// TODO: create the ps struct
-	ps := PubSub{topics : make(map[string][]chan string)}
-
-
-
+	ps := PubSub{topics: make(map[string][]chan string)}
 
 	// TODO: create the arrays of messages to be sent on each topic
 	var beesArray [5]string
@@ -105,18 +102,15 @@ func main() {
 
 	// TODO: create the publisher goroutines
 
-	go publisher(ps,"bees", beesArray)
-	go publisher(ps,"philosophy", philosophyArray)
-
+	go ps.publisher("bees", beesArray)
+	go ps.publisher("philosophy", philosophyArray)
 
 	// TODO: create the subscriber goroutines
-	go subscriber(ps,"nature","bees")
-	go subscriber(ps,"humanities", "philosophy")
+	go ps.subscriber("nature", "bees")
+	go ps.subscriber("humanities", "philosophy")
 
 	// TODO: wait for all publishers to be done
 
 	wg.Wait()
-
-
 
 }
